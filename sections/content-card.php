c8ec6b68c42d108c06b111e5d6b61a26b83c4e25
jomelle.lastrilla <jllastrilla@gmail.com>
2024-09-01 22:02:04 +0800

    add: section content files

---
+++ sections/content-card.php
@@ -0,0 +1,7 @@
+<div class="lo-website-item">
+  <a href="#" alt="" target="_blank">
+    <img src="" title=""/>
+  </a>
+  <span class="lo-website-category">Website Category</span>
+  <a href="#">Website Title</a>
+</div>
---
+++ sections/content-filter.php
@@ -0,0 +1,7 @@
+<select>
+  <option value="taxonomy_id_1">Website Category 1</option>
+  <option value="taxonomy_id_2">Website Category 1</option>
+  <option value="taxonomy_id_3">Website Category 1</option>
+  <option value="taxonomy_id_4">Website Category 1</option>
+  <option value="taxonomy_id_5">Website Category 1</option>
+</select>
