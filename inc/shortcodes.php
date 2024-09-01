7ab64daeed998f9a6ced5e78f5fac2368b2ea799
jomelle.lastrilla <jllastrilla@gmail.com>
2024-09-01 23:01:13 +0800

    Update shortcodes.php

--- inc/shortcodes.php
+++ inc/shortcodes.php
@@ -3,8 +3,31 @@
 function lo_website_listings_function(){
   ob_start();
 
-  get_template_part('section/content' , 'listings');
+
+  $args = array();
   
+  $args = array(
+    'post_type'       => 'website-projects', 
+    'order'           => 'ASC', 
+    'orderby'         => 'title',
+    'post_status'     => array('publish')
+  );
+
+  $query = new WP_Query($args);
+
+  $count = $query->found_posts;
+
+  if ( $query->have_posts() ) :
+    ob_start(); 
+    while ( $query->have_posts() ) : $query->the_post();  
+    
+      get_template_part('section/content' , 'listings');
+
+    endwhile;
+  endif;
+
+  
+
   $content = ob_get_clean();
 
   return $content;
