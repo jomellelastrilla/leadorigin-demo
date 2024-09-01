a38837c30c65bab560e86af96515271ba99df078
jomelle.lastrilla <jllastrilla@gmail.com>
2024-09-01 22:13:41 +0800

    Create content-listings.php

---
+++ sections/content-listings.php
@@ -0,0 +1,19 @@
+<div class="lo-website-listings">
+
+  <!-- Load content-filter.php -->
+  <?php get_template_part('sections/content' , 'filter'); ?>
+
+  <div id="lo-website-results">  
+    <!-- Load content-card.php -->
+    <?php get_template_part('sections/content' , 'card'); ?>
+    <?php get_template_part('sections/content' , 'card'); ?>
+    <?php get_template_part('sections/content' , 'card'); ?>
+    <?php get_template_part('sections/content' , 'card'); ?>
+    <?php get_template_part('sections/content' , 'card'); ?>
+    <?php get_template_part('sections/content' , 'card'); ?>
+    <?php get_template_part('sections/content' , 'card'); ?>
+    <?php get_template_part('sections/content' , 'card'); ?>
+
+  </div>
+
+</div>
