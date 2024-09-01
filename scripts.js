04b70231e576fc705d73f565728267af5e30f484
jomelle.lastrilla <jllastrilla@gmail.com>
2024-09-02 00:23:13 +0800

    update: script.js
    add ajax call

---
+++ scripts.js
@@ -0,0 +1,46 @@
+var $ = jQuery.noConflict();
+
+$(function(){
+
+  function fetchListings(category){
+    return new Promise((resolve, reject) => {
+      const { action, nonce, ajax_url }  = LO_DATA;
+
+      const data = {
+        action, //name of wordpress action
+        nonce,
+        ajax_url,
+        category
+      };
+
+
+      $.ajax({
+        type: "POST",
+        url: ajax_url,
+        data,
+        dataType: "json",
+        beforeSend: function () {},
+      })
+        .done((response) => {
+          resolve(response);
+        })
+        .fail(function (e) {
+          reject(e.message);
+        })
+        .always(function () {});
+    });
+  }
+
+  $('#lo-website-filter').on('change', function(){
+    const category = $(this).val();
+
+     
+    fetchListings(category).then(({success, data}) => {
+      if (success) {
+
+      }
+    })
+
+  });
+
+});
