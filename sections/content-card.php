085db3140a4c240203c76429123404edde8791dd
jomelle.lastrilla <jllastrilla@gmail.com>
2024-09-01 23:18:25 +0800

    Update content-card.php

--- sections/content-card.php
+++ sections/content-card.php
@@ -1,6 +1,6 @@
 <div class="lo-website-item">
   <a href="#" alt="" target="_blank">
-    <img src="" title=""/>
+    <img src="https://placehold.co/535x300?text=Website\nProject" title="test text"/>
   </a>
   <span class="lo-website-category">Website Category</span>
   <a href="#">Website Title</a>
